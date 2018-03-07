6cde852c2ca64b570418d3255a29612042148f3c
Miles Winston <miles.winston.biz@gmail.com>
2018-03-07 07:17:37 +0000

    Added last of CRUD and comment functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,41 @@ class PostsController < ApplicationController
         @post = Post.all.order('created_at DESC')
     end 
     
+    def show
+        @post = Post.find(params[:id])
+    end
+    
     def new
+        @post = Post.new
     end 
     
     def create
         @post = Post.new(post_params)
-        @post.save
-        
-        redirect_to @post
+        if @post.save
+            redirect_to @post
+        else 
+            render 'new'
+        end 
     end 
     
-    def show
+    def edit 
         @post = Post.find(params[:id])
+    end 
+    
+    def update
+    @post = Post.new(post_params)
+        if @post.update(post_params)
+            redirect_to @post
+        else 
+            render 'edit'
+        end  
+    end 
+    
+    def destroy
+         @post = Post.find(params[:id])
+         @post.destroy
+         
+         redirect_to posts_path
     end 
     
     private 
